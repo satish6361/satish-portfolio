adb8ef32290efa349f4e27266a798406c698dbba
Satish <ssatishbalathe@gmail.com>
2025-02-14 23:43:57 +0530

    Resume and details updated

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,17 +1,19 @@
 export const personalData = {
   name: "SATISH",
-  profile: '/profile.png',
+  profile: "/profile.png",
   designation: "Software Developer",
-  description: "My name is SATISH. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
-  email: 'ssatishbalathe@gmail.com',
-  phone: '+916361597930',
-  address: 'Bengaluru, India - 560100 ',
-  github: 'https://github.com/satish6361',
-  facebook: 'https://www.facebook.com/',
-  linkedIn: 'https://www.linkedin.com/in/satishbalathe/',
-  twitter: 'https://twitter.com/',
-  stackOverflow: 'https://stackoverflow.com/',
+  description:
+    "My name is SATISH. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
+  email: "ssatishbalathe@gmail.com",
+  phone: "+916361597930",
+  address: "Bengaluru, India - 560100 ",
+  github: "https://github.com/satish6361",
+  facebook: "https://www.facebook.com/",
+  linkedIn: "https://www.linkedin.com/in/satishbalathe/",
+  twitter: "https://twitter.com/",
+  stackOverflow: "https://stackoverflow.com/",
   leetcode: "https://leetcode.com/satish6361/",
   devUsername: "satish6361",
-  resume: "https://drive.google.com/file/d/1C40pEPpq2LIRLiMhz71RHvPIOW57v0YD/view?usp=drive_link"
-}
+  resume:
+    "https://drive.google.com/file/d/10yUQag1FhYqGv-To8fDqtrzXXbK1P0_d/view?usp=sharing",
+};
